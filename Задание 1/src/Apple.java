3e788f7cfa7cc67f60e72f9c8bb871e68aa49171
palvanovahmet <58997688+palvanovahmet@users.noreply.github.com>
2019-12-19 21:39:17 +0300

    Add files via upload

---
+++ Задание 1/src/Apple.java
@@ -0,0 +1,24 @@
+
+public class Apple extends Food {
+
+	private String size;
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public void consume() {
+		System.out.println(this+" Сьедено");
+
+	}
+	public Apple(String size){
+		super("Яблоко");
+		this.size=size;
+	}
+	public String toString() {
+		return super.toString()+" Размера '" +size.toUpperCase()+"'";
+	}
+	
+}
---
+++ Задание 2/src/Мекан/Main.java
@@ -0,0 +1,14 @@
+package zadanie 2;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		formula frame = new formula();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
